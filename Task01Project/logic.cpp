0623198a958792a359f0ce489ad3f3e79b128977
KrutalevichLev <krutaleviclev@gmail.com>
2026-04-06 16:43:00 +0300

    Add project files.

---
+++ Task01Project/logic.cpp
@@ -0,0 +1,16 @@
+﻿#include "logic.h"
+
+int sum(int number) {
+	int count = 0;
+	if (number < 0) {
+		return -1;
+	}
+	if (number == 0) {
+		return 0;
+	}
+
+	if (number > 0) {
+		return number + sum(number - 1);
+	}
+}
+
