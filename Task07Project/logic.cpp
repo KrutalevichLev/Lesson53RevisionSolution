e8ef7dadf93d6fda3c0fae8d21fc84803259d739
KrutalevichLev <krutaleviclev@gmail.com>
2026-04-06 18:04:27 +0300

    Task07

--- Task07Project/logic.cpp
+++ Task07Project/logic.cpp
@@ -2,4 +2,18 @@
 // Количество нечётных цифр
 //
 // Необходимо спроектировать и реализовать программу, 
-// которая считает количество нечётных цифр в заданном целом числе.
+// которая считает количество нечётных цифр
+// в заданном целом числе.
+
+int count_of_odd_digits(int number) {
+	number = number < 0 ? -number : number;
+
+	if (number == 0 || number == 10) {
+		return 0;
+	}
+	int last_digit = number % 10;
+
+	int counter = last_digit % 2 != 0 ? 1 : 0;
+
+	return counter + count_of_odd_digits(number / 10);
+}
