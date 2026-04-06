6d1926bd875aeee36849d70b64d59392dac7f037
KrutalevichLev <krutaleviclev@gmail.com>
2026-04-06 17:33:22 +0300

    Task05

--- Task05Project/logic.cpp
+++ Task05Project/logic.cpp
@@ -4,3 +4,16 @@
 // Необходимо спроектировать и реализовать программу, 
 // которая подсчитывает количество цифр 
 // в заданном целом числе.
+
+int count(int number) {
+	if (number == 0) {
+		return 1;
+	}
+	number = number < 0 ? -number : number;
+
+	if (number < 10) {
+		return 1;
+	}
+
+	return 1 + count(number / 10);
+}
