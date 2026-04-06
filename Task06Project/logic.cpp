da493821f82a2fc4a82b95d87d9c2d18d732e7ba
KrutalevichLev <krutaleviclev@gmail.com>
2026-04-06 17:46:41 +0300

    Task06

--- Task06Project/logic.cpp
+++ Task06Project/logic.cpp
@@ -3,3 +3,19 @@
 //
 // Необходимо спроектировать и реализовать программу, 
 // которая вычисляет сумму чётных чисел от 1 до N.
+#include "logic.h"
+
+int sum(int number) {
+	int count = 0;
+	if (number == 1 || number == 0) {
+		return 0;
+	}
+	if (number < 0) {
+		return -1;
+	}
+	number = number % 2 != 0 ? number - 1 : number;
+
+	return number + sum(number - 2);
+
+}
+
